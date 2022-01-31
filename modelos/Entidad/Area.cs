5619eae3abd13207435c01180722763de81b930d
Elianb7 <61747736+Elianb7@users.noreply.github.com>
2022-01-30 22:50:12 -0500

    Add files via upload

---
+++ modelos/Entidad/Area.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.Entidad
+{
+    class Area
+    {
+        public int id_Area { get; set; }
+        public string nombre_Area { get; set; }
+        public string DescripcionArea { get; set; }
+    }
+}
